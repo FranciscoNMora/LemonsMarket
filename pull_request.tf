d2e9454149b5324e52afce45c0199b00e96f55af
FranciscoNMora <navionavio@hotmail.com>
2022-05-16 14:43:42 +0200

    Correct subnet

--- pull_request.tf
+++ pull_request.tf
@@ -30,22 +30,10 @@ resource "aws_subnet" "demosubnet" {
   vpc_id                  = "${aws_vpc.demovpc.id}"
   cidr_block             = "${var.subnet_cidr}"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "Demo subnet"
-  }
-}
-
-# Creating 2nd subnet
-resource "aws_subnet" "demosubnet1" {
-  vpc_id                  = "${aws_vpc.demovpc.id}"
-  cidr_block             = "${var.subnet1_cidr}"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1"
-
-  tags = {
-    Name = "Demo subnet 1"
   }
 }
 
