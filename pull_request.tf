6c18f7052edf973cf11c42ae07890abf4185ffc1
FranciscoNMora <navionavio@hotmail.com>
2022-05-16 14:38:50 +0200

    Precreated key-pairs

--- pull_request.tf
+++ pull_request.tf
@@ -122,12 +122,6 @@ resource "aws_security_group" "demosg" {
   }
 }
 
-# Creating key pair
-resource "aws_key_pair" "demokey" {
-  key_name   = "${var.key_name}"
-  public_key = "${file(var.public_key)}"
-}
-
 # Creating EC2 Instance
 resource "aws_instance" "demoinstance" {
 
@@ -144,7 +138,7 @@ resource "aws_instance" "demoinstance" {
   count= "${var.master_count}"
 
   # SSH key that we have generated above for connection
-  key_name = "${aws_key_pair.demokey.id}"
+  key_name = "terraform-test"
 
   # Attaching security group to our instance
   vpc_security_group_ids = ["${aws_security_group.demosg.id}"]
@@ -169,31 +163,31 @@ resource "aws_instance" "demoinstance" {
   }
 
   # SSH into instance
-  connection {
-
-    # Host name
-    host = self.public_ip
-    # The default username for our AMI
-    user = "ec2-user"
-    # Private key for connection
-    private_key = "${file(var.private_key)}"
-    # Type of connection
-    type = "ssh"
-  }
-
-  # Installing splunk on newly created instance
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo amazon-linux-extras install docker -y",
-      "sudo service docker start",
-      "sudo usermod -a -G docker ec2-user",
-      "sudo chkconfig docker on",
-      "sudo yum install -y git",
-      "sudo chmod 666 /var/run/docker.sock",
-      "docker pull dhruvin30/dhsoniweb:v1",
-      "docker run -d -p 80:80 dhruvin30/dhsoniweb:latest"
-  ]
- }
+#  connection {
+#
+#    # Host name
+#    host = self.public_ip
+#    # The default username for our AMI
+#    user = "ec2-user"
+#    # Private key for connection
+#    private_key = "${file(var.private_key)}"
+#    # Type of connection
+#    type = "ssh"
+#  }
+#
+#  # Installing splunk on newly created instance
+#  provisioner "remote-exec" {
+#    inline = [
+#      "sudo yum update -y",
+#      "sudo amazon-linux-extras install docker -y",
+#      "sudo service docker start",
+#      "sudo usermod -a -G docker ec2-user",
+#      "sudo chkconfig docker on",
+#      "sudo yum install -y git",
+#      "sudo chmod 666 /var/run/docker.sock",
+#      "docker pull dhruvin30/dhsoniweb:v1",
+#      "docker run -d -p 80:80 dhruvin30/dhsoniweb:latest"
+#  ]
+# }
 }
 
