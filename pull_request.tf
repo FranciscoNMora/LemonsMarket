49b7aa2d50c48589fdd81f13eb4cbecc811bada5
Francisco Navio <67550919+FranciscoNMora@users.noreply.github.com>
2022-05-16 18:45:30 +0200

    Update pull_request.tf
    Static test

--- pull_request.tf
+++ pull_request.tf
@@ -3,113 +3,6 @@ provider "aws" {
   region     = "${var.aws_region}"
 }
 
-## Creating VPC
-#resource "aws_vpc" "demovpc" {
-#  cidr_block       = "${var.vpc_cidr}"
-#  instance_tenancy = "default"
-#
-#  tags = {
-#    Name = "Demo VPC"
-#  }
-#}
-#
-## Creating Internet Gateway
-#resource "aws_internet_gateway" "demogateway" {
-#  vpc_id = "${aws_vpc.demovpc.id}"
-#}
-#
-## Grant the internet access to VPC by updating its main route table
-#resource "aws_route" "internet_access" {
-#  route_table_id         = "${aws_vpc.demovpc.main_route_table_id}"
-#  destination_cidr_block = "0.0.0.0/0"
-#  gateway_id             = "${aws_internet_gateway.demogateway.id}"
-#}
-#
-## Creating 1st subnet
-#resource "aws_subnet" "demosubnet" {
-#  vpc_id                  = "${aws_vpc.demovpc.id}"
-#  cidr_block             = "${var.subnet_cidr}"
-#  map_public_ip_on_launch = true
-#  availability_zone = "us-east-1a"
-#
-#  tags = {
-#    Name = "Demo subnet"
-#  }
-#}
-
-# Creating Security Group
-#resource "aws_security_group" "demosg" {
-#  name        = "Demo Security Group"
-#  description = "Demo Module"
-#  vpc_id      = "${aws_vpc.demovpc.id}"
-#
-#  # Inbound Rules
-#  # HTTP access from anywhere
-#  ingress {
-#    from_port   = 80
-#    to_port     = 80
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  # HTTPS access from anywhere
-#  ingress {
-#    from_port   = 443
-#    to_port     = 443
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  # SSH access from anywhere
-#  ingress {
-#    from_port   = 22
-#    to_port     = 22
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  # Splunk default port
-#  ingress {
-#    from_port   = 8000
-#    to_port     = 8000
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  # Replication Port
-#  ingress {
-#    from_port   = 8089
-#    to_port     = 8089
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  # Management Port
-#  ingress {
-#    from_port   = 4598
-#    to_port     = 4598
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  # Ingestion Port
-#  ingress {
-#    from_port   = 9997
-#    to_port     = 9997
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  # Outbound Rules
-#  # Internet access to anywhere
-#  egress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#}
-
 # Creating EC2 Instance
 resource "aws_instance" "demoinstance" {
 
@@ -133,7 +26,7 @@ resource "aws_instance" "demoinstance" {
 
   # Attaching Tag to Instance
   tags = {
-    Name = "${var.GITHUB_REF}"
+    Name = "Example"
   }
 
   # Root Block Storage
